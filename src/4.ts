ca71a6834056efb85603dd2c376f1a833afab9ac
Viacheslav Haber <gaberslavik@gmail.com>
2023-09-02 17:05:19 +0300

    finished 4 task

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,47 @@
-// const key = new Key();
+class Key {
+	private signature: string;
+	constructor() {
+		this.signature = (Math.random() * 10).toFixed(4);
+	}
+	getSignature() {
+		return this.signature;
+	}
+}
+class Person {
+	private key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+	getKey(this: Person) {
+		return this.key;
+	}
+}
+abstract class House {
+	protected door: boolean = false;
+	key: Key;
+	private tenants: Person[] = [];
+	constructor(key: Key) {
+		this.key = key;
+	}
+	public comeIn(person: Person) {
+		if (!this.door) return;
+		this.tenants.push(person);
+	}
+	public abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+	public openDoor(key: Key) {
+		return (this.door = key.getSignature() === this.key.getSignature() ? true : false);
+	}
+}
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+const key = new Key();
 
-// house.openDoor(person.getKey());
+const house = new MyHouse(key);
+const person = new Person(key);
 
-// house.comeIn(person);
+house.openDoor(person.getKey());
 
-// export {};
+house.comeIn(person);
+
+export {};
